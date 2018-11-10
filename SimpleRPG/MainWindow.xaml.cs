a9f0b9b7a9c2eca3d7ed3e5c0cc7d9c9c96f6a4a
Alexander Kristensen <sheepsaurus@gmail.com>
2018-11-10 01:13:41 +0100

    First Commit - Pre-15.1

--- SimpleRPG/MainWindow.xaml.cs
+++ SimpleRPG/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
+﻿using System.Windows;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SimpleRPG
 {
@@ -20,9 +15,25 @@ namespace SimpleRPG
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Player player;
         public MainWindow()
         {
             InitializeComponent();
+            player = new Player
+            { 
+                CurrentHitPoints = 10,
+                MaximumHitPoints = 10,
+                Gold = 20,
+                ExperiencePoints = 0,
+                Level = 1
+            };
+
+            hitpointsValueLabel.Content = player.CurrentHitPoints.ToString();
+            goldValueLabel.Content = player.Gold.ToString();
+            experienceValueLabel.Content = player.ExperiencePoints.ToString();
+            levelValueLabel.Content = player.Level.ToString();
+
+            Location location = new Location(1, "Home", "This is your house");
         }
     }
 }
